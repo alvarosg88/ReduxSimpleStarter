63ab9d1b1768b7b5bdd5689b3ff10856c0645702
vertebrand <alvarosg88@gmail.com>
2017-05-11 21:42:32 +0200

    basic state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/*class App extends React.Component {
-    render() {
-        return <h1>Hello, Asshole!!!</h1>;
-    }
-}*/
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBkmvC9HKZUYYzHlbKlo5ZxaE3RVFDri2k';
 
 const App = () => {
-    return <h1>Hi!</h1>;
+    return  (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
